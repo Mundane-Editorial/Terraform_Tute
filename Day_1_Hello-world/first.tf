1592b0800e126219ef9cc9e91b50b7479399afa3
Mundane-Editorial <uday2k17@rediffmail.com>
2024-07-18 13:15:59 +0530

    Day 1 - Hello world

--- Day_1_Hello-world/first.tf
+++ Day_1_Hello-world/first.tf
@@ -6,3 +6,7 @@
 output hello1{
     value = "Hello world"
 }
+
+
+// Now to run this program, simply we'll use
+ /*     "terraform plan" :   this command is used to check what kind what type plan is coded for building the infrastructure     */
