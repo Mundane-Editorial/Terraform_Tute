29d2c32f1a299d15e9fe4e4e55b43aa93eebd298
Mundane-Editorial <uday2k17@rediffmail.com>
2024-07-18 14:11:15 +0530

    Day - 1 : multiple block in single tf file

---
+++ Day_1/Multiple_block_in_single_tf_file/multiple_block_tute.tf
@@ -0,0 +1,11 @@
+output "first_output_block" {
+    value = "first_output_block"
+}
+
+output "second_output_block" {
+    value = "second_output_block"
+}
+
+output "third_output_block" {
+    value = "third_output_block"
+}
