bd5cec52a43673fb3f503364e9efbfb670089fdd
Mundane-Editorial <uday2k17@rediffmail.com>
2024-07-24 21:29:23 +0530

    Day_5 commit: terraform console command

---
+++ Day_5/Terraform_console_command/repositories.tf
@@ -0,0 +1,13 @@
+resource "github_repository" "terraform_test_repo" {
+  name = "terraform_test_repo"
+  visibility = "private"
+  auto_init = true
+}
+
+
+output "terraform_test_repo_url" {
+  value = github_repository.terraform_test_repo.html_url
+}
+
+
+// read info.txt within same directory
---
+++ Day_5/Terraform_console_command/variables.tf
@@ -0,0 +1,15 @@
+variable "github_token" {
+  
+}
+
+variable "name" {   
+  default = "Mundane Editorial"
+}
+
+variable "age" {
+  default = 25
+}
+
+variable "city" {
+  default = "mohali"
+}
